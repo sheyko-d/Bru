c2ab64b8f59e076ff8853cde3cdcc596386e00a1
Dmitry <sheyko.dmitriy@gmail.com>
2017-03-26 22:29:02 +0300

    Fix UI in the warning screen

--- app/src/main/java/com/moyersoftware/bru/main/MainActivity.java
+++ app/src/main/java/com/moyersoftware/bru/main/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         updateGoogleToken();
         initAds();
 
-        if (!Util.isTutorialShown()) {
+        if (true || !Util.isTutorialShown()) {
             startActivity(new Intent(this, WarningActivity.class));
             finish();
         } else if (!Util.isLoggedIn()) {
--- app/src/main/java/com/moyersoftware/bru/warning/WarningActivity.java
+++ app/src/main/java/com/moyersoftware/bru/warning/WarningActivity.java
@@ -4,7 +4,6 @@ import android.animation.ArgbEvaluator;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -45,20 +44,9 @@ public class WarningActivity extends AppCompatActivity {
         setContentView(R.layout.activity_warning);
 
         bindViews();
-        initStatusBar();
         initActionBar();
         initViewPager();
         Util.setTutorialShown();
-    }
-
-    /**
-     * Makes the status bar transparent.
-     */
-    private void initStatusBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
     }
 
     private void bindViews() {
